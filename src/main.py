b1572b68b51c81453e74720fe8f5f26527806143
Mj-lol <michaelpics52@gmail.com>
2024-10-14 17:38:17 -0500

    generation impending

--- src/main.py
+++ src/main.py
@@ -25,13 +25,47 @@ def copy_directory(src_dir, dest_dir):
             print(f"Created directory: {dest_path}")
             copy_directory(src_path, dest_path)
 
+def extract_title(markdown):
+    lines = markdown.split('\n')
+    for line in lines:
+        if line.startswith('# '):
+            return line[2:].strip()
+    raise Exception("H1 heading required")
+
+
+def generate_page(fro, template, to):
+    print(f"Generating page from {fro} to {to} using {template}.")
+    with open(fro, 'r', encoding='utf-8') as f:
+        markdown_content = f.read()
+    with open(template, 'r', encoding='utf-8') as f:
+        template_content = f.read()
+    html_node = markdown_to_html_node(markdown_content)
+    html_content = html_node.to_html()
+    title = extract_title(markdown_content)
+    full_html = template_content.replace('{{ Title }}', title).replace('{{ Content }}', html_content)
+
+    dest_dir = os.path.dirname(to)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    with open(to, 'w', encoding='utf-8') as f:
+        f.write(full_html)
+
+
+
+
+
 def main():
     src_dir = 'static'
     dest_dir = 'public'
-
+    template = 'template.html'
     clean_destination(dest_dir)
     copy_directory(src_dir, dest_dir)
+    #alternatively we have
+    #shutil.copytree(src_dir, dest_dir)
+    src ="content/index.md"
+    dest="public/index.html"
     print("Copy operation completed successfully.")
+    generate_page(src, template, dest)
 
 if __name__ == '__main__':
     main()
